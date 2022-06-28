45f409f32a153c890d2b70d50ed47ac6d9655c78
gomachuca <jjorgemachuca@gmail.com>
2022-06-28 19:22:40 +0200

    .
    .

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -10,4 +10,25 @@ router.get("/create", (req, res, next) => {
   res.render("movies/new-movie.hbs");
 });
 
+
+
+router.post("/create", (req, res) => {
+  const { name } = req.body;
+  Celebrity.findOne({ name })
+    .then((celebritiesDocFromDB) => {
+      if (!celebritiesDocFromDB) {
+        // prettier-ignore
+        User.create({ name })
+        .then(() => res.redirect('/movies'));
+      } else {
+        res.render("movies/movies.hbs", {
+          message: "It seems you are already registered. ☀️",
+        });
+        return;
+      }
+    })
+    .catch((err) => console.log(`Error while creating a new user: ${err}`));
+});
+
+
 module.exports = router;
