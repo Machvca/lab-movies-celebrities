416d44a1077e868d5cfb5139519c4d1655062591
gomachuca <jjorgemachuca@gmail.com>
2022-06-28 15:45:29 +0200

    av
    av

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,21 +1,21 @@
 const router = require("express").Router();
 
 /* GET celebrities page */
-router.get("/celebrities", (req, res, next) => {
-  res.render("celebrities");
+router.get("/", (req, res, next) => {
+  res.render("celebrities/celebrities.hbs");
 });
 
-router.get("/celebrities/create", (req, res, next) => {
-  res.render("celebrities/new-celebrity");
+router.get("create", (req, res, next) => {
+  res.render("celebrities/new-celebrity.hbs");
 });
 
-router.post("/celebrities/create", (req, res, next) => {
+router.post("/create", (req, res, next) => {
   const { name, occupation, catchPhrase } = req.body;
   Drone.create({ name, occupation, catchPhrase })
     .then(() => res.redirect("/celebrities"))
     .catch((error) => {
       console.log(error);
-      res.render("celebrities/new-celebrity");
+      res.render("celebrities/new-celebrity.hbs");
     });
 });
 
